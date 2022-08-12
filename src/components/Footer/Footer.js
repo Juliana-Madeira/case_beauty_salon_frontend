fec4ae26d9e5f36f8725798a906e237358f56fc0
Juliana Madeira <juliwood1980@gmail.com>
2022-08-12 18:22:03 -0300

    footer sc

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import styled from "styled-components";
 
-import './Footer.css';
+const FooterBlock = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--primary);
+  color: var(--lightText);
+  height: 3.75em;
+`
+const FooterSpan = styled.span`
+  font-size: 0.9em;
+  font-weight: lighter;
+`
 
 const Footer = () => {
   return (
-    <div className='footer'>
-        <span>@Case Beauty Salon. All rights reserved.</span>
-    </div>
+    <FooterBlock>
+        <FooterSpan>@Case Beauty Salon. All rights reserved.</FooterSpan>
+    </FooterBlock>
   )
 }
 
